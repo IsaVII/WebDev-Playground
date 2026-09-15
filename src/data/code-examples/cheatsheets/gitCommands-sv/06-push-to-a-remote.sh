7253ffded3449992e3a43c23b245df4417d3db97
Isa <isasturm7@gmail.com>
2026-09-15 06:48:23 +0200

    Update Git command cheeat sheet with better explainations

---
+++ src/data/code-examples/cheatsheets/gitCommands-sv/06-push-to-a-remote.sh
@@ -0,0 +1,8 @@
+# Första push på en ny branch - talar också om för Git att komma ihåg denna remote-branch
+git push -u origin <branch>
+
+# t.ex.
+git push -u origin main
+
+# Varje push efter det, från samma branch
+git push
---
+++ src/data/code-examples/cheatsheets/gitCommands/06-push-to-a-remote.sh
@@ -0,0 +1,8 @@
+# First push on a new branch - also tells Git to remember this remote branch
+git push -u origin <branch>
+
+# e.g.
+git push -u origin main
+
+# Every push after that, from the same branch
+git push
